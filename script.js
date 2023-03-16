662a18f132e44889857428be6fb1942f4e8230cd
Pranav <pranav.techiegeek@gmail.com>
2023-03-16 18:09:34 +0530

    unsplash + technologies

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
+const backgroundImage = "https://codedamn.com/assets/cover-image.svg";
 const name = document.querySelector("#name");
 const profileImage = document.querySelector("#profile-image");
+let coverImage = document.querySelector("#cover-image");
+const aboutElement = document.querySelector("#about");
 
+async function setCoverImage(about) {
+	if (about === "Learning to code on codedamn is fun & Interactive") {
+		coverImage.style.backgroundImage = `url("${backgroundImage}")`;
+		return undefined;
+	}
+
+	const response = await fetch(
+		`https://api.unsplash.com/photos/random?query=${about}&orientation=landscape&client_id=VKfNE7ps_SXcHWyvQhL6Ouziatpahe3lMditkleWa8I`
+	).then((response) => response.json());
+	const topics = await fetch(
+		`https://api.unsplash.com/topics?client_id=RDrltY4xtGTzIL9k1AdbuuZjCJl3iqztOIBA9KChtx4`
+	).then((response) => response.json());
+
+	console.log(topics);
+	console.log(coverImage.style.backgroundImage);
+	console.log(response.urls.raw);
+	coverImage.style.backgroundImage = `url("${response.urls.raw}fit=crop&crop=entropy")`;
+	console.log(coverImage.style.backgroundImage);
+}
 async function setProfileImage(name) {
 	if (name === "codedamn") {
 		return false;
@@ -23,3 +45,4 @@ async function setProfileImage(name) {
 }
 
 setProfileImage(name.textContent);
+setCoverImage(aboutElement.textContent.trim());
